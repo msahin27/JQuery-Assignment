6908573a46db67e57fd655cb890072bcb4d6a596
msahin27 <52358179+msahin27@users.noreply.github.com>
2019-08-07 16:52:36 +0200

    Week 8 JQuery

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 // Make the project great again with jQuery ..
+
+
+
+// DELETE TOPICS
+$(document).on("click", ".delete", function(e) {
+  e.preventDefault();
+  $(this)
+    .parent()
+    .remove();
+});
+
+
+//HIDE TOPICS
+
+$('#hide').click(function () {
+    $('ul').toggle(this.unchecked);
+  });
+
+
+// ADD TOPIC TO LIST
+$("form").submit(function(e) {
+  if ($(".input").val() !== "") {
+    
+    let newTask = $(".input").val();
+    
+    $("ul").append(
+      "<li class='list-group-item'><span class='name'>" + newTask +
+        "</span><span class='delete'>delete</span>"
+    );
+
+    $(".input").val("");
+  }
+
+  return false;
+});
+
+
+// SEARCH INPUT
+$("#search-topics input").keyup(function() {
+  const searchingTopics = $(this).val();
+
+  $("ul > li ").each(function() {
+    const liName = $(this).text();
+    showLi = liName.toUpperCase().indexOf(searchingTopics.toUpperCase()) !== -1;
+    $(this).toggle(showLi);
+  });
+});
